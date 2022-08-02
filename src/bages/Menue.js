f3483944d06a14b2d5915e80f836a0ceebdbd9dd
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-08-02 17:14:40 +0200

    done active filter

--- src/bages/Menue.js
+++ src/bages/Menue.js
@@ -4,56 +4,18 @@ import HeaderMenue from '../components/menue/HeaderMenue';
 import ButtonMenue from '../components/menue/ButtonMenue';
 import MenuFilter from '../components/menue/MenuFilter';
 import DataMenue from '../components/menue/DataMenue';
-import All from"../img/menue/all.png"
-import pizza from"../img/menue/pizza.png" 
-import salad from"../img/menue/salad.png" 
-import pasta from"../img/menue/pasta.png" 
-import dessert from"../img/menue/dessert.png" 
+
 import Customize from '../components/menue/Customize';
 
 const Menue = () => {
   //state data button
  const [menueData, setMenueData] = useState(DataMenue)
-  const [stateActive , setStateActive] = useState({
-    filterMeal : [
-      {
-        id:1,
-        name:"all",
-        filterShow:()=>FilterButton("All"),
-        image:All
-      },
-      {
-        id:2,
-        name:"pizza",
-        filterShow:()=>FilterButton("pizza"),
-        image:pizza
-      },
-      {
-        id:3,
-        name:"salad",
-        filterShow:()=>FilterButton("salad" ),
-        image:salad
-      },
-      {
-        id:4,
-        name:"pasta",
-        filterShow:()=>FilterButton("pasta"),
-        image:pasta
-      },
-      {
-        id:5,
-        name:"dessert",
-        filterShow:()=>FilterButton("dessert"),
-        image:dessert
-      },
-
-    ]
-  })
+  
 
   //click filter Button
   const FilterButton = (button , index)=>{
   const DataFilterMenue = DataMenue.filter((item)=>item.type == button)
-  if (button === "All") {
+  if (button === "all") {
     setMenueData(DataMenue)
     return;
   }else{
@@ -66,7 +28,7 @@ const Menue = () => {
   return (
     <MenueStyle>
       <HeaderMenue />
-      <ButtonMenue stateActive={stateActive} />
+      <ButtonMenue  FilterButton={FilterButton}  />
       <MenuFilter menueData={menueData}/>
       <Customize menueData={menueData} />
     </MenueStyle>
--- src/components/menue/ButtonMenue.js
+++ src/components/menue/ButtonMenue.js
@@ -1,14 +1,53 @@
 import React  from 'react'
+import { useEffect ,useState} from 'react'
 import styled from 'styled-components'
+import All from"../../img/menue/all.png"
+import pizza from"../../img/menue/pizza.png" 
+import salad from"../../img/menue/salad.png" 
+import pasta from"../../img/menue/pasta.png" 
+import dessert from"../../img/menue/dessert.png" 
 
+const ButtonMenue = ({ stateActive ,FilterButton }) => { 
+  //onClick={item.filterShow }
+  const [stateData , setStateData] = useState({
+    active:null,
+    ButtonsFilter : [
+     { id: 1, name: "all", title: "all" ,img:All },
+     { id: 2, name: "pizza", title: "pizza" ,img:pizza },
+     { id: 3, name: "pasta", title: "pasta",img:pasta },
+     { id: 3, name: "salad", title: "salad",img:salad },
+     { id: 3, name: "dessert", title: "dessert",img:dessert },
 
-const ButtonMenue = ({ stateActive }) => { 
-
+     
+  ]
+   })
+ //index element put in => active
+ const HandelIndex = (index) =>{
+  setStateData({...stateData , active:stateData.ButtonsFilter[index]})
+ }
+ //HandellClassName
+ const HandellClassName = (index) =>{
+  if (stateData.ButtonsFilter[index] === stateData.active) {
+   return "active"
+  }else{
+   return "unactive"
+  }
+ }
+ //click filter && css active
+ function HandelClickFilter(item , index) {
+  FilterButton(item.name)
+  HandelIndex(index)
+ 
+ }
+ //useEffict start select All work
+ useEffect(() => { 
+  setStateData({...stateData , active:stateData.ButtonsFilter[0]})
+ }, [])
   return (
     <MainButton >
-      {stateActive.filterMeal.map((item,index) =>{
+      {stateData.ButtonsFilter.map((item,index) =>{
         return (
-          <button key={index} className="button active"  onClick={item.filterShow }  ><img src={item.image}/>{item.name}</button>
+          <button onClick={()=>HandelClickFilter(item ,index)} className={HandellClassName(index)} ><img src={item.img}/>{item.name}</button>
         )
       })}
     </MainButton>
@@ -21,25 +60,33 @@ display: flex;
 justify-content: center;
 flex-wrap: wrap;
 background-color: var(--red-color);
+.unactive{
+  color: #ffffff7b;
+  img{
+    opacity: 0.4;
+    border: 6px solid #ffffff7b;
+  }
+}
+.active{
+  color: white;
+  img{
+    opacity: 1;
+    border: 6px solid white;
+  }
 
- .button{
+
+}
+ button{
 
     all: unset;
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 30px;
-    color: #ffffff7b;
     cursor: pointer;
-
     img{
     width: 80px;
-    border: 6px solid #ffffff7b;
     border-radius: 50%;
-    opacity: 0.7;
-      :link {
-        border: 6px solid white;
-      }
     }
     @media (max-width:833px) {
     padding: 15px;
