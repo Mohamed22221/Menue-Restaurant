3a708eda24bdcb1c156e242cf7430903bd1fb131
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-08-12 22:59:31 +0200

    done head slider , done  responsive

--- src/components/menue/headslide/HeaderSlider.js
+++ src/components/menue/headslide/HeaderSlider.js
@@ -4,8 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./HeaderSlider.scss"
 import DataMenue from '../../../components/menue/DataMenue';
-import calory from "../../../img/glopal/calories.png"
-import cheese from "../../../img/glopal/cheese.png"
+import calory from "../../../img/glopal/calorie.png"
+import cheese from "../../../img/glopal/cheese2.png"
 import ButtonOrder from '../../glopalComponent/ButtonOrder';
 const HeaderSlider = () => {
 
@@ -35,32 +35,32 @@ const HeaderSlider = () => {
         {DataMenue.filter((item) => item.type == "pizza").map((item) => {
           return (
             <div className={`main-item ${item.type}${item.id}`} >
-                  <div className='col-slider'>
-                    <h1>{item.name}</h1>
-                    <span className='rating'>{item.rating}</span>
-                    <p>{item.discription}</p>
-                    <div className='main-about'>
-                      <div className='calory'>
-                        <img src={calory} />
-                        <div className='about'>
-                          <h4>480</h4>
-                          <p>Calories</p>
-                        </div>
-                      </div>
-                      <div className='cheese'>
-                        <img src={cheese} />
-                        <div className='about'>
-                          <h4>120g</h4>
-                          <p>Mozarella</p>
-                        </div>
-                      </div>
-
-                    </div>
-                    <div className='fotter-slider1'>
-                    <ButtonOrder item={`/meal/${item.id}`} title="Order" />
-                      <h2>{item.price}$</h2>
+              <div className='col-slider'>
+                <h1>{item.name}</h1>
+                <span className='rating'>{item.rating}</span>
+                <p>{item.discription}</p>
+                <div className='main-about'>
+                  <div className='calory'>
+                    <img src={calory} />
+                    <div className='about'>
+                      <h4>480</h4>
+                      <p>Calories</p>
                     </div>
                   </div>
+                  <div className='cheese'>
+                    <img src={cheese} />
+                    <div className='about'>
+                      <h4>120g</h4>
+                      <p>Mozarella</p>
+                    </div>
+                  </div>
+
+                </div>
+                <div className='fotter-slider1'>
+                  <ButtonOrder item={`/meal/${item.id}`} title="Order" />
+                  <h2>{item.price}$</h2>
+                </div>
+              </div>
             </div>
           )
 
